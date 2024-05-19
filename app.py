3d0f419433360e3a7fa28d286be81ab2d2b372f1
Rody Haket <rhaket@student.tudelft.nl>
2024-05-19 14:59:57 +0200

    Added training data endpoint

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ import numpy as np
 import joblib
 import gdown
 import os 
+import csv
 from flasgger import Swagger, LazyJSONEncoder
 from flasgger import swag_from
 from pathlib import Path
@@ -63,6 +64,31 @@ def predict():
     result = jsonify({'prediction': prediction.flatten().tolist()})
     return result
 
+
+@swag_from("docs/add.yaml" )
+@app.route('/add', methods=['POST'])
+def add():
+    """
+    Add training data.
+
+    Returns:
+        result (json) : JSON response.
+    """
+    url = request.json['url']
+    label = request.json['label']
+
+    # Path to the CSV file
+    csv_file_path = f"{os.getenv('SAVE_TRAINING_DATA_FOLDER')}{os.getenv('SAVE_TRAINING_DATA_FILENAME')}"
+
+    # Ensure the data directory exists
+    os.makedirs(os.path.dirname(csv_file_path), exist_ok=True)
+
+    # Append data to the CSV file
+    with open(csv_file_path, mode='a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([url, label])
+
+    return jsonify({'msg': f'Thank you for submitting:\n{url} with label {label}'})
 
 inference = Inference()
 preprocessor = Preprocessing()
